f2f613b2254791122305b2b83744d6358153b68b
SoMuchKarma <joeacq72@gmail.com>
2025-07-11 13:14:33 -0400

    Testing Ability to send messages to eachother

--- client.py
+++ client.py
@@ -1,4 +1,17 @@
 import socket
+import threading
+
+def handle_receive(client_socket):
+    while True:
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        print("\nServer:", data.decode())
+
+def handle_send(client_socket):
+    while True:
+        msg = input("You: ")
+        client_socket.sendall(msg.encode())
 
 # DO NOT REMOVE CODE UNDERNEATH-- CRUCIAL TO CONNECTION
 host = 'localhost' 
@@ -8,6 +21,8 @@ client_socket.connect((host, port))
 # UP TO HERE
 
 print("Connected to server.")
+threading.Thread(target=handle_receive, args=(client_socket,), daemon=True).start()
+handle_send(client_socket)
 
 #Closes the connection when done
 client_socket.close()
--- server.py
+++ server.py
@@ -1,6 +1,19 @@
 import socket
-
+import threading
 # BOOT UP SERVER.PY THEN CLIENT.PY IN SEPERATE TERMINALS TO AFFIRM CONNECTION
+
+#Code to send and receive messages, end using Ctrl+C
+def handle_receive(conn):
+    while True:
+        data = conn.recv(1024)
+        if not data:
+            break
+        print("\nClient:", data.decode())
+
+def handle_send(conn):
+    while True:
+        msg = input("You: ")
+        conn.sendall(msg.encode())
 
 print("Starting server.py")
 
@@ -13,6 +26,9 @@ conn, addr = server_socket.accept()
 # UP TO HERE
 
 print("Connected by", addr)
+
+threading.Thread(target=handle_receive, args=(conn,), daemon=True).start()
+handle_send(conn)
 
 # Closes the connection when done
 conn.close()

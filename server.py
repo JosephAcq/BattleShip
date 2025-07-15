d4ff52d4b0cc5c4545f3f0bf89280817ad26a5f2
MattiWaki <wakiwaya.matti@gmail.com>
2025-07-15 16:26:15 -0400

    Update server.py
    changed server socket so it allows to be connected by any computer, not just your own

--- server.py
+++ server.py
@@ -31,7 +31,7 @@ print("Starting server.py")
 
 # DO NOT REMOVE CODE UNDERNEATH-- CRUCIAL TO CONNECTION
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('localhost', 5050)) 
+server_socket.bind(('0.0.0.0', 5050))
 server_socket.listen(1)
 print("Waiting for connection...")
 conn, addr = server_socket.accept()
